4f8d8936c4dfcd3cc70c5e00ce175abb2b145926
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2024-07-08 13:02:41 -0400

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,34 @@
+# Printing
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+# Storing strings in variables
+my_var = "Hello world"
+
+# Printing a string by adding
+print(my_var + " and Vaman")
+
+# Printing a string by using commas to stitch them together
+print(my_var, "and Vaman")
+
+# Printing a string with an f-string
+print(f"{my_var} and Vaman")
+
+# Getting user input
+name = input("Enter your name: ")
+print(name)
+
+# Integers in Python
+num = 1
+
+# This line will NOT modify the num variable's value
+print(num + 1)
+
+# This will still print 1 as the num variable has not changed
+print(num)
+
+# Integer Input
+num = int(input("Enter a number: "))
+print(num + 1)

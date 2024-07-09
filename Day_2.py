3b7b0ea342d9feb3f6d50c4bed32a034c73bf842
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2024-07-09 13:11:46 -0400

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,32 @@
+# Replacing parts of a string
+my_str = "Hello World"
+
+my_str = my_str.replace("l", "n")
+
+print(my_str)
+
+# Comparison Operators
+# ==
+# !=
+# >
+# >=
+# <
+# <=
+
+# Basic conditional
+num = 1
+if num >= 2:
+    print("num is greater than 2")
+
+else:
+    print("num is less than 2")
+
+# Even or Odd Checker
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is even")
+
+else:
+    print(f"{num} is odd")
+
